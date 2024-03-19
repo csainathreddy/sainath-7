9105a345e6ec5ec6f788ceeaabaa5828ed897a5b
csainathreddy <162982760+csainathreddy@users.noreply.github.com>
2024-03-19 09:08:28 +0530

    Add files via upload

---
+++ pythonfile.7.py
@@ -0,0 +1,129 @@
+#2.) python progrm to determine if
+# the give sets are Disjoint
+#or Not without using Inbuilt-in Functons
+
+
+set1 = {'python','java',Data science'}
+set2 = {'ML','AI,'R Language','python'}
+set3 = {'Data Analytion','RObOtion','Deep Learning'}
+ c = 0
+        flag=0
+        for val in range(3):
+        c=c+1
+        if c==1:
+        for val in set1:
+        if val in set or val in set3:
+        flag=1
+        break
+    if c==2:
+        for val in set1 or val in set3:
+
+            if c==3:
+                for val in set3:
+                    if val in set or val in set1:
+                        flag=1
+                        break
+                    if flage==0
+                    print("Disjoint")
+                    else:
+                        print("joint")
+    #3.)
+       list1 = ["M","na","i","ke"]
+       list2 = ["y","me","s","lly"]
+
+
+       # 13 = []
+       # for val in range (len(list)):
+       #    ans = list[val]+list[val]
+       #  13.append(ans)
+       # print(13)
+
+
+
+      # i=0
+       #while i<len(list1):
+        #   13.append(list1[i]+list[i])
+         #  i+=1
+         #print(13)
+
+
+   # Hacker rank ---> Caesar chipher
+   # ! Functions
+   #? DEF
+   # Function is black of code which is used to perform a specifi
+   # Function can be created using def keyword
+
+
+   #? Function has 3 parts
+   #  Function declaration
+   #  Function defanition
+   #  Function call
+
+    def greet():
+        print ("Welcom User!!")
+        greet()
+        #greet() #function calling
+
+
+
+        # !Eg:2
+        # ? Function with parameter
+        def greeting (name):
+            print("Welcome",name)
+
+            for val in gange(3)
+            username = input("Enter the name")
+            greeting(username) # username is argument
+
+
+             # !Eg:3
+             #1.)write a python script to generate and print a diction to
+             # contains a number (between 1 and n) in the from(x,x*x).
+             # sample Dictionary (n = 5)
+             #Expect output :{1:1,2:4,3:9,4:16,5:25}
+
+             #2.) find the uncommon word from 2 string
+             
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+         
+           
+
+
+
+           
+
+
+
+       
+
+
+       
+
+
+
+
+
+
+
+
+                          
+        
